a6df816cf8654caacf9c07e6fd6bda4cb91980a1
Myeongwon André Jeon <andrejeon@Myeongwons-MBP.fios-router.home>
2020-09-18 14:05:06 -0400

    book service test complete

--- bookstore/src/test/java/com/lambdaschool/bookstore/services/BookServiceImplTest.java
+++ bookstore/src/test/java/com/lambdaschool/bookstore/services/BookServiceImplTest.java
@@ -2,6 +2,8 @@ package com.lambdaschool.bookstore.services;
 
 import com.lambdaschool.bookstore.BookstoreApplication;
 import com.lambdaschool.bookstore.exceptions.ResourceNotFoundException;
+import com.lambdaschool.bookstore.models.Book;
+import com.lambdaschool.bookstore.models.Wrote;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,6 +12,10 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = BookstoreApplication.class)
@@ -22,11 +28,19 @@ public class BookServiceImplTest
     @Autowired
     private BookService bookService;
 
+    @Autowired
+    private SectionService sectionService;
+
     @Before
     public void setUp() throws
             Exception
     {
         MockitoAnnotations.initMocks(this);
+
+        List<Book> myList = bookService.findAll();
+        for (Book b : myList) {
+            System.out.println(b.getBookid() + " " + b.getTitle());
+        }
     }
 
     @After
@@ -38,25 +52,38 @@ public class BookServiceImplTest
     @Test
     public void a_findAll()
     {
+        assertEquals(5, bookService.findAll().size());
     }
 
     @Test
     public void b_findBookById()
     {
+        assertEquals("Flatterland", bookService.findBookById(26).getTitle());
     }
 
     @Test(expected = ResourceNotFoundException.class)
     public void c_notFindBookById()
     {
+        assertEquals("", bookService.findBookById(420).getTitle());
     }
 
     @Test
     public void d_delete()
     {
+        bookService.delete(27);
+        assertEquals(4, bookService.findAll().size());
     }
 
     @Test
     public void e_save()
     {
+        Book b1 = new Book();
+        b1.setSection(sectionService.findSectionById(21));
+        b1.setTitle("test");
+        b1.setIsbn("12345678");
+        b1.setBookid(0);
+        b1.setCopy(2020);
+        Book addBook = bookService.save(b1);
+        assertEquals("test", addBook.getTitle());
     }
 }
